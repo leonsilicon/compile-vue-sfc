1f839ea583396ede6660fa6384f8c70a7019519b
Leon Si <contact@leonzalion.com>
2022-04-26 13:21:32 -0400

    fix: add vite as dep

--- scripts/build.ts
+++ scripts/build.ts
@@ -7,8 +7,3 @@ rmDist();
 exec('tsc');
 exec('tsc-alias');
 await copyPackageFiles({ commonjs: { external: /vue-tsc\// } });
-
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-const pkgJson = JSON.parse(fs.readFileSync('dist/package.json', 'utf8'));
-pkgJson.dependencies.vite = 'npm:ignoredep@^2.5.1';
-fs.writeFileSync('dist/package.json', JSON.stringify(pkgJson, null, '\t'));
